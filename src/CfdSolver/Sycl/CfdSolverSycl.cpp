94faa463d5a0a6ec2ff43a6f59a47a900e2a1054
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2019-06-07 19:58:51 +0200

    Add initial SYCL support

---
+++ src/CfdSolver/Sycl/CfdSolverSycl.cpp
@@ -0,0 +1,142 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2019, Christoph Neuhauser, Stefan Haas, Paul Ng
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstring>
+#include "BoundaryValuesSycl.hpp"
+#include "UvwSycl.hpp"
+#include "SorSolverSycl.hpp"
+#include "CfdSolverSycl.hpp"
+
+void CfdSolverSycl::initialize(const std::string &scenarioName,
+        Real Re, Real Pr, Real omg, Real eps, int itermax, Real alpha, Real beta, Real dt, Real tau,
+        Real GX, Real GY, Real GZ, bool useTemperature, Real T_h, Real T_c,
+        int imax, int jmax, int kmax, Real dx, Real dy, Real dz,
+        Real *U, Real *V, Real *W, Real *P, Real *T, uint32_t *Flag) {
+    this->scenarioName = scenarioName;
+    this->Re = Re;
+    this->Pr = Pr;
+    this->omg = omg;
+    this->eps = eps;
+    this->itermax = itermax;
+    this->alpha = alpha;
+    this->beta = beta;
+    this->dt = dt;
+    this->tau = tau;
+    this->GX = GX;
+    this->GY = GY;
+    this->GZ = GZ;
+    this->useTemperature = useTemperature;
+    this->T_h = T_h;
+    this->T_c = T_c;
+    this->imax = imax;
+    this->jmax = jmax;
+    this->kmax = kmax;
+    this->dx = dx;
+    this->dy = dy;
+    this->dz = dz;
+
+    // Create all arrays for the simulation.
+    this->U = new Real[(imax+1)*(jmax+2)*(kmax+2)];
+    this->V = new Real[(imax+2)*(jmax+1)*(kmax+2)];
+    this->W = new Real[(imax+2)*(jmax+2)*(kmax+1)];
+    this->P = new Real[(imax+2)*(jmax+2)*(kmax+2)];
+    this->T = new Real[(imax+2)*(jmax+2)*(kmax+2)];
+    this->T_temp = new Real[(imax+2)*(jmax+2)*(kmax+2)];
+    this->F = new Real[(imax+1)*(jmax+1)*(kmax+1)];
+    this->G = new Real[(imax+1)*(jmax+1)*(kmax+1)];
+    this->H = new Real[(imax+1)*(jmax+1)*(kmax+1)];
+    this->RS = new Real[(imax+1)*(jmax+1)*(kmax+1)];
+    this->Flag = new FlagType[(imax+2)*(jmax+2)*(kmax+2)];
+
+    // Copy the content of U, V, W, P, T and Flag to the internal representation.
+    memcpy(this->U, U, sizeof(Real)*(imax+1)*(jmax+2)*(kmax+2));
+    memcpy(this->V, V, sizeof(Real)*(imax+2)*(jmax+1)*(kmax+2));
+    memcpy(this->W, W, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+1));
+    memcpy(this->P, P, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+2));
+    memcpy(this->T, T, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+2));
+    memcpy(this->Flag, Flag, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+2));
+}
+
+CfdSolverSycl::~CfdSolverSycl() {
+    delete[] U;
+    delete[] V;
+    delete[] W;
+    delete[] P;
+    delete[] T;
+    delete[] T_temp;
+    delete[] F;
+    delete[] G;
+    delete[] H;
+    delete[] RS;
+    delete[] Flag;
+}
+
+void CfdSolverSycl::setBoundaryValues() {
+    setBoundaryValuesSycl(T_h, T_c, imax, jmax, kmax, U, V, W, T, Flag);
+}
+
+void CfdSolverSycl::setBoundaryValuesScenarioSpecific() {
+    setBoundaryValuesScenarioSpecificSycl(scenarioName, imax, jmax, kmax, U, V, W, Flag);
+}
+
+Real CfdSolverSycl::calculateDt() {
+    calculateDtSycl(Re, Pr, tau, dt, dx, dy, dz, imax, jmax, kmax, U, V, W, useTemperature);
+    return dt;
+}
+
+
+void CfdSolverSycl::calculateTemperature() {
+    calculateTemperatureSycl(Re, Pr, alpha, dt, dx, dy, dz, imax, jmax, kmax, U, V, W, T, T_temp, Flag);
+}
+
+void CfdSolverSycl::calculateFgh() {
+    calculateFghSycl(Re, GX, GY, GZ, alpha, beta, dt, dx, dy, dz, imax, jmax, kmax, U, V, W, T, F, G, H, Flag);
+}
+
+void CfdSolverSycl::calculateRs() {
+    calculateRsSycl(dt, dx, dy, dz, imax, jmax, kmax, F, G, H, RS);
+}
+
+
+void CfdSolverSycl::executeSorSolver() {
+    sorSolverSycl(omg, eps, itermax, dx, dy, dz, imax, jmax, kmax, P, RS, Flag);
+}
+
+void CfdSolverSycl::calculateUvw() {
+    calculateUvwSycl(dt, dx, dy, dz, imax, jmax, kmax, U, V, W, F, G, H, P, Flag);
+}
+
+void CfdSolverSycl::getDataForOutput(Real *U, Real *V, Real *W, Real *P, Real *T) {
+    // Copy the content of U, V, W, P, T in the internal representation to the specified output arrays.
+    memcpy(U, this->U, sizeof(Real)*(imax+1)*(jmax+2)*(kmax+2));
+    memcpy(V, this->V, sizeof(Real)*(imax+2)*(jmax+1)*(kmax+2));
+    memcpy(W, this->W, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+1));
+    memcpy(P, this->P, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+2));
+    memcpy(T, this->T, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+2));
+    memcpy(Flag, this->Flag, sizeof(Real)*(imax+2)*(jmax+2)*(kmax+2));
+}
---
+++ src/CfdSolver/Sycl/UvwSycl.cpp
@@ -0,0 +1,64 @@
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2019, Christoph Neuhauser, Stefan Haas, Paul Ng
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "UvwSycl.hpp"
+
+void calculateFghSycl(
+        Real Re, Real GX, Real GY, Real GZ, Real alpha, Real beta,
+        Real dt, Real dx, Real dy, Real dz, int imax, int jmax, int kmax,
+        Real *U, Real *V, Real *W, Real *T, Real *F, Real *G, Real *H, FlagType *Flag) {
+    // TODO
+}
+
+void calculateRsSycl(
+        Real dt, Real dx, Real dy, Real dz, int imax, int jmax, int kmax,
+        Real *F, Real *G, Real *H, Real *RS) {
+    // TODO
+}
+
+void calculateDtSycl(
+        Real Re, Real Pr, Real tau,
+        Real &dt, Real dx, Real dy, Real dz, int imax, int jmax, int kmax,
+        Real *U, Real *V, Real *W,
+        bool useTemperature) {
+    // TODO
+}
+
+void calculateUvwSycl(
+        Real dt, Real dx, Real dy, Real dz, int imax, int jmax, int kmax,
+        Real *U, Real *V, Real *W, Real *F, Real *G, Real *H, Real *P, FlagType *Flag) {
+    // TODO
+}
+
+void calculateTemperatureSycl(
+        Real Re, Real Pr, Real alpha,
+        Real dt, Real dx, Real dy, Real dz,
+        int imax, int jmax, int kmax,
+        Real *U, Real *V, Real *W, Real *T, Real *T_temp, FlagType *Flag) {
+    // TODO
+}
