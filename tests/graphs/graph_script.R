ee35ab2bb00b115c197411e8d1810a8fd86785b7
Stefan Haas <stefan.haas@drei.at>
2019-07-18 10:51:21 +0200

    inserted results in csvs, updatet R Scipt and Graphs

--- tests/graphs/graph_script.R
+++ tests/graphs/graph_script.R
@@ -30,3 +30,32 @@ ggsave('graphs/runtime_rayleigh_benard_convection_8-2-1_solver.pdf', p, width=7,
 p <- printGraph("results/rayleigh_benard_convection_8-2-1_runtime_platforms.csv", "Platforms", "Platform", "Precision")
 ggsave('graphs/runtime_rayleigh_benard_convection_8-2-1_platforms.svg', p, width=7, height=4, units='in')
 ggsave('graphs/runtime_rayleigh_benard_convection_8-2-1_platforms.pdf', p, width=7, height=4, units='in')
+
+
+#Speedup Graph
+data <- read.csv("results/speedup_OpenMP_double.csv", check.names=FALSE, header=TRUE, sep=',')
+
+for(i in 1:nrow(data)) 
+{
+  data[i, "speedup"] <- data[data$threads == 1, "time"]/data[i, "time"]
+}
+levels(data$group)
+#data$group <- factor(data$group, levels = rev(levels(data$group)))
+data
+
+p <- ggplot(data=data, mapping=aes(x=threads, y=speedup))
+p <- p + scale_colour_manual(values=c("#333333","#E37222","#0065BD", "#A2AD00"))
+p <- p + labs(title = "Speedup OpenMP")
+p <- p + labs(x = "Threads", y = "Speedup")
+p <- p + guides(color = guide_legend(reverse=TRUE))
+p <- p + geom_line()
+p <- p + geom_point()
+p <- p + scale_x_continuous(breaks=c(4,8,16,28))
+p <- p + scale_y_continuous(breaks=c(2,4,6,8,10,12,14,16))
+p <- p + theme_tufte()
+p <- p + theme(plot.title = element_text(hjust = 0.5))
+p <- p + theme(plot.subtitle = element_text(hjust = 0.5))
+p
+
+ggsave('graphs/speedup_OpenMP_double.svg', p, width=7, height=4, units='in')
+ggsave('graphs/speedup_OpenMP_double.pdf', p, width=7, height=4, units='in')
