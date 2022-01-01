6affa1aade756c596a14a26f16900abac3a63858
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2022-01-02 00:34:33 +0100

    Removed RKF45 code.

--- src/ParticleTracer/StreamlineTracer.cpp
+++ src/ParticleTracer/StreamlineTracer.cpp
@@ -71,7 +71,6 @@ void traceStreamlineParticle(
                 currentTrajectory, gridOrigin, gridSize, imax, jmax, kmax, dx, dy, dz, U, V, W, P, T);
 
         // Integrate to the new position using Runge-Kutta of 4th order.
-
         rvec3 k1 = dt * getVectorVelocityAt(particlePosition, gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
         rvec3 k2 = dt * getVectorVelocityAt(particlePosition + k1/Real(2.0), gridOrigin, gridSize, imax, jmax, kmax,
                 U, V, W);
@@ -79,44 +78,6 @@ void traceStreamlineParticle(
                 U, V, W);
         rvec3 k4 = dt * getVectorVelocityAt(particlePosition + k3, gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
         particlePosition = particlePosition + k1/Real(6.0) + k2/Real(3.0) + k3/Real(3.0) + k4/Real(6.0);
-
-
-        // Integrate to the new position using the Runge-Kutta-Fehlberg method (RKF45).
-        // For more details see: https://maths.cnam.fr/IMG/pdf/RungeKuttaFehlbergProof.pdf
-        /*const Real tol = Real(2.0 * 1e-5);
-        rvec3 approximationRK4, approximationRK5;
-        bool timestepNeedsAdaptation = false;
-        do {
-            rvec3 k1 = dt * getVectorVelocityAt(particlePosition, gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
-            rvec3 k2 = dt * getVectorVelocityAt(
-                    particlePosition + k1*Real(1.0/4.0),
-                    gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
-            rvec3 k3 = dt * getVectorVelocityAt(
-                    particlePosition + k1*Real(3.0/32.0) + k2*Real(9.0/32.0),
-                    gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
-            rvec3 k4 = dt * getVectorVelocityAt(
-                    particlePosition + k1*Real(1932.0/2197.0) - k2*Real(7200.0/2197.0) + k3*Real(7296.0/2197.0),
-                    gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
-            rvec3 k5 = dt * getVectorVelocityAt(
-                    particlePosition + k1*Real(439.0/216.0) - k2*Real(8.0) + k3*Real(3680.0/513.0)
-                    - k4*Real(845.0/4104.0),
-                    gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
-            rvec3 k6 = dt * getVectorVelocityAt(
-                    particlePosition - k1*Real(8.0/27.0) + k2*Real(2.0) - k3*Real(3544.0/2565.0)
-                    + k4*Real(1859.0/4104.0) - k5*Real(11.0/40.0),
-                    gridOrigin, gridSize, imax, jmax, kmax, U, V, W);
-            approximationRK4 = particlePosition + k1*Real(25.0/216.0) + k3*Real(1408.0/2565.0)
-                                     + k4*Real(2197.0/4101.0) - k5*Real(1.0/5.0);
-            approximationRK5 = particlePosition + k1*Real(16.0/135.0) + k3*Real(6656.0/12825.0)
-                                     + k4*Real(28561.0/56430.0) - k5*Real(9.0/50.0) + k6*Real(2.0/55.0);
-            Real s = std::pow(tol*dt / (Real(2.0) * glm::length(approximationRK5 - approximationRK4)), Real(1.0/4.0));
-            if (s < 0.9 || s > 1.1) {
-                timestepNeedsAdaptation = true;
-            }
-            dt = s*dt;
-        } while(timestepNeedsAdaptation);
-        particlePosition = approximationRK4;*/
-
 
         Real segmentLength = glm::length(particlePosition - oldParticlePosition);
         lineLength += segmentLength;
